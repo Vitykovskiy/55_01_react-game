c1f05f1ceb860025b8e0fb4bd0a3d08876a96bf7
Alix1982 <alix1982@yandex.ru>
2025-11-13 18:17:20 +0300

    fix_import

--- packages/client/src/pages/mainPage/index.ts
+++ packages/client/src/pages/mainPage/index.ts
@@ -1,1 +1,1 @@
-import { MainPage } from './ui/MainPage'
+export { MainPage } from './ui/MainPage'
--- packages/client/src/shared/ui/buttonCustom/index.tsx
+++ packages/client/src/shared/ui/buttonCustom/index.tsx
@@ -1,13 +1,1 @@
-import { ButtonCustom, ButtonCustomProps } from './ButtonCustom'
-
-// type typeButton = 'button' | 'submit' | 'reset'
-// type viewButton = 'action' | 'outlined'
-
-// export type ButtonCustomProps = {
-//   name: string
-//   text: string
-//   handleClick?: () => void
-//   // handleClick: (name:string) => void
-//   type?: typeButton
-//   view?: viewButton
-// }
+export { ButtonCustom, type ButtonCustomProps } from './ButtonCustom'
