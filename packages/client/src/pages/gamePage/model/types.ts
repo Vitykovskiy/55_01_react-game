aa5c65c0a3994d8e15e853695b32b6e5175972d7
Alix1982 <alix1982@yandex.ru>
2025-11-15 12:16:20 +0300

    layout_startGame

--- packages/client/src/pages/gamePage/model/types.ts
+++
@@ -1,8 +0,0 @@
-import { Dispatch, SetStateAction } from 'react'
-
-export type StatusGame = 'start' | 'game' | 'end'
-
-export type StartGameProps = {
-  statusGame?: StatusGame
-  setStatusGame: Dispatch<SetStateAction<StatusGame>>
-}
--- packages/client/src/pages/gamePage/ui/startGame/StartGame.tsx
+++ packages/client/src/pages/gamePage/ui/startGame/StartGame.tsx
@@ -1,50 +1,49 @@
 import Layout from '@shared/ui/Layout'
-import { StartGameProps } from '../../model/types'
-import { Button, Text } from '@gravity-ui/uikit'
+import { Text } from '@gravity-ui/uikit'
 import s from './StartGame.module.scss'
-import backgroundStartPage from '../../../../public/game/startGame.png'
-import Section from '@shared/ui/Section'
+import { Buttons } from './buttons/Buttons'
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { RoutePath } from '@shared/config/routing'
 
-export const StartGame = ({ statusGame, setStatusGame }: StartGameProps) => {
-  const handleClick = () => {
-    setStatusGame('game')
+export const StartGame = () => {
+  const [isCounter, setIsCounter] = useState(false)
+  const [counter, setCounter] = useState(3)
+
+  const navigete = useNavigate()
+  const setCounterContent = () => {
+    return setCounter(counter - 1)
   }
+
+  useEffect(() => {
+    isCounter &&
+      (counter > 0
+        ? setTimeout(setCounterContent, 1000)
+        : navigete(RoutePath.Main))
+  }, [isCounter, counter])
+
   return (
     <div className={s.startGame} id={s.startGame}>
       <Layout title="начало игры">
-        {/* <Section> */}
-        {/* <img className={s.background} src={backgroundStartPage} alt='фон'/> */}
-        {/* <Section> */}
-        <Text as="p" className={s.text} variant="body-2">
-          В Magic Type вам нужно быстро и точно печатать появляющиеся на экране
-          заклинания-слова: каждое правильно набранное слово мгновенно
-          уничтожает врага, продвигающегося к вам; если промедлите или ошибётесь
-          — противники приблизятся и нанесут урон. Сосредоточьтесь на
-          приоритетных целях, тренируйте слепой набор, поддерживайте ритм без
-          остановок и используйте редкие усиления, возникающие при быстрой серии
-          точных слов — так вы дольше выживете и пройдёте уровни с максимальным
-          счётом.
-        </Text>
-
-        {/* <Button
-              className={s.startGame__button}
-              view="action"
-              width='max'
-              onClick={handleClick}
-            >
-              Начать играть
-            </Button>
-            <Button
-              className={s.startGame__button}
-              view="outlined"
-              width='max'
-              onClick={handleClick}
-            >
-              Назад
-            </Button> */}
-
-        {/* </Section> */}
-        {/* </Section> */}
+        {isCounter ? (
+          <Text as="p" className={s.textCounter} variant="display-4">
+            {counter}
+          </Text>
+        ) : (
+          <>
+            <Text as="p" className={s.text} variant="body-2">
+              В Magic Type вам нужно быстро и точно печатать появляющиеся на
+              экране заклинания-слова: каждое правильно набранное слово
+              мгновенно уничтожает врага, продвигающегося к вам; если промедлите
+              или ошибётесь — противники приблизятся и нанесут урон.
+              Сосредоточьтесь на приоритетных целях, тренируйте слепой набор,
+              поддерживайте ритм без остановок и используйте редкие усиления,
+              возникающие при быстрой серии точных слов — так вы дольше выживете
+              и пройдёте уровни с максимальным счётом.
+            </Text>
+            <Buttons setIsCounter={setIsCounter} />
+          </>
+        )}
       </Layout>
     </div>
   )
