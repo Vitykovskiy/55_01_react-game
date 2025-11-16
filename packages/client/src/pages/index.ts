b61147c11b4e6a4b97450248702a3f77d52195ce
volynaa <110327482+volynaa@users.noreply.github.com>
2025-11-16 14:12:10 +0300

    #20 Добавление страницы лидерборда (#41)
    * Реализация страницы лидерборд
    
    * Реализация страницы лидерборд
    
    * Реализация страницы лидерборд
    
    * Реализация страницы лидерборд
    
    * Реализация страницы лидерборд
    
    * Реализация страницы лидерборд
    
    * Реализация страницы лидерборд

--- packages/client/src/pages/index.ts
+++ packages/client/src/pages/index.ts
@@ -1,6 +1,6 @@
 export { ForumPage } from './ForumPage'
 export { GamePage } from './GamePage'
-export { LeaderboardPage } from './LeaderboardPage'
+export { LeaderboardPage } from './Leaderboard'
 export { LoginPage } from './LoginPage'
 export { MainPage } from './mainPage/ui/MainPage'
 export { ErrorPage } from './errorPage/ui/ErrorPage'
