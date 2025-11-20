355ed5346e200110dbaece8b47b77baddfb2117c
volynaa <volynskiy.nik@mail.ru>
2025-11-20 21:34:54 +0300

    Реализация страницы профиль

--- packages/client/src/pages/ProfilePage/lib/changePassword.ts
+++ packages/client/src/pages/ProfilePage/lib/changePassword.ts
@@ -2,5 +2,5 @@ import { PasswordChangeData } from '../model/types'
 import { changePasswordApi } from '../api'
 
 export const changePassword = (data: PasswordChangeData) => {
-  changePasswordApi(data)
+  return changePasswordApi(data)
 }
