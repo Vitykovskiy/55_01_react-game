e6f1a8d9616442ac8c05f720b46bf929f2686fa0
Александр <81099741+alix1982@users.noreply.github.com>
2025-11-29 13:39:12 +0300

    62 Добавить Service Workers (#69)
    * fix_build
    
    * add_service_worker
    
    * workbox
    
    * serviceWorker_noLibrary
    
    * fix_readme
    
    * delete_comment
    
    * set_smaller version_workbox-build
    
    * fix_bug_review
    
    * fix_bug_review

---
+++ packages/client/src/app/lib/serviceWorker/generate-sw.js
@@ -0,0 +1,19 @@
+import { injectManifest } from 'workbox-build';
+
+const MAX_FILE_SIZE_CACHE = 5242880;
+
+const build = async () => {
+  const { count, size } = await injectManifest({
+    swSrc: './src/app/lib/serviceWorker/sw-template.js',
+    swDest: './dist/client/sw.js',
+    globDirectory: './dist',
+    globPatterns: [
+      '**/*.{js,css,html,png,svg,jpg,ico}'
+    ],
+    maximumFileSizeToCacheInBytes: MAX_FILE_SIZE_CACHE, // 5MB
+  });
+
+  console.log(`✅ Generated SW: ${count} files, ${size} bytes`);
+}
+
+build();
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite'
 const srcPath = path.resolve(__dirname, './src')
 dotenv.config()
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: Number(process.env.CLIENT_PORT) || 3000,
