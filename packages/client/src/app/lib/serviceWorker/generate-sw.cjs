0e8bd36c9f4129f25446edb21965d3dcc02f27b0
Reghan <reghansan@gmail.com>
2025-12-02 00:58:27 +0300

    Пофикшен билд

--- packages/client/src/app/lib/serviceWorker/generate-sw.cjs
+++ packages/client/src/app/lib/serviceWorker/generate-sw.cjs
@@ -1,19 +1,17 @@
-import { injectManifest } from 'workbox-build';
+const { injectManifest } = require('workbox-build')
 
-const MAX_FILE_SIZE_CACHE = 5242880;
+const MAX_FILE_SIZE_CACHE = 5242880
 
 const build = async () => {
   const { count, size } = await injectManifest({
     swSrc: './src/app/lib/serviceWorker/sw-template.js',
     swDest: './dist/client/sw.js',
     globDirectory: './dist',
-    globPatterns: [
-      '**/*.{js,css,html,png,svg,jpg,ico}'
-    ],
+    globPatterns: ['**/*.{js,css,html,png,svg,jpg,ico}'],
     maximumFileSizeToCacheInBytes: MAX_FILE_SIZE_CACHE, // 5MB
-  });
+  })
 
-  console.log(`✅ Generated SW: ${count} files, ${size} bytes`);
+  console.log(`✅ Generated SW: ${count} files, ${size} bytes`)
 }
 
-build();
+build()
