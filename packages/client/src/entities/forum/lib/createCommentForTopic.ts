51816fadc5a6bb97f5da495aec209ea3a89388ea
Vitykovskiy <vitykovskiy@gmail.com>
2026-01-26 13:05:14 +0300

    Фиксы после ревью 2

--- packages/client/src/entities/forum/lib/createCommentForTopic.ts
+++ packages/client/src/entities/forum/lib/createCommentForTopic.ts
@@ -5,16 +5,18 @@ import { commentDtoToView } from './mappers'
 export async function createCommentForTopic(
   topicId: number,
   text: string,
-  parentCommentId = null
+  parentCommentId?: number
 ): Promise<ForumTopicComment | undefined> {
   try {
     const response = await createCommentForTopicRequest(topicId, {
       content: text.trim(),
-      parentCommentId,
+      parentCommentId: parentCommentId ?? null,
     })
+
     if (!response) {
       throw new Error('Сервер вернул пустой ответ')
     }
+
     return commentDtoToView(response)
   } catch (err) {
     console.error('Ошибка создания комментария', err)
--- packages/client/src/entities/forum/lib/mappers.ts
+++ packages/client/src/entities/forum/lib/mappers.ts
@@ -3,6 +3,7 @@ import { ForumTopicComment } from '../model/types'
 
 export const commentDtoToView = (comment: CommentDto): ForumTopicComment => ({
   id: comment.id,
+  // TODO: Убрать заглушки после мержа https://github.com/Vitykovskiy/55_01_react-game/issues/121
   firstName: 'Пользователь',
   lastName: String(comment.userId),
   avatarUrl: '',
