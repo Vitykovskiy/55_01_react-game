fea7ac4a4c32c425ed9b78ee71047804dd8345c3
volynaa <volynskiy.nik@mail.ru>
2026-02-03 22:10:49 +0300

    Добавление emoji

--- packages/client/src/entities/forum/lib/mappers.ts
+++ packages/client/src/entities/forum/lib/mappers.ts
@@ -8,6 +8,7 @@ import {
 
 export const commentDtoToView = (comment: CommentDto): ForumTopicComment => ({
   id: comment.id,
+  // TODO: Убрать заглушки после мержа https://github.com/Vitykovskiy/55_01_react-game/issues/121
   firstName: 'Пользователь',
   lastName: String(comment.userId),
   avatarUrl: '',
